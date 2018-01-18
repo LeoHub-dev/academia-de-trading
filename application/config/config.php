6dae3f4ec67de5dfdf9ca9cbe3b623fca6b203da
Douglas Nieves <douglasjosenieves@gmail.com>
2018-01-18 10:42:51 -0400

    Ignore

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'https://academiadetrading.net/';
-$config['base_url'] = 'http://192.168.1.2/circulo/';
+$config['base_url'] = 'http://localhost/academiadetrading/';
 
 /*
 |--------------------------------------------------------------------------
